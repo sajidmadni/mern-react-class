c5feb528e80c85e417223ce7cc2460d4666f399f
Muhammad Sajid Madni <sajid.madni@catalyst-uk.com>
2023-01-01 08:31:10 +0500

    Add bootstrap and products listing

---
+++ src/pages/about-us.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import { Col } from 'react-bootstrap';
+
+const AboutUs =  () => {
+  return (
+    <div>  
+        <Container>
+            <Row className='mt-30'>
+                <Col>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </Col>
+            </Row>
+        </Container>
+    </div>
+
+  )
+}
+
+export default AboutUs;
